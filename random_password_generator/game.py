0b1aa18bd7e44c59892a4104e70b095b9a7ba04e
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-09 19:38:00 +0530

    added password_generator

---
+++ random_password_generator/game.py
@@ -0,0 +1,35 @@
+# collect user preferences:
+# - length
+# - should contain uppercase
+# - should contain digits
+# - should contain special
+
+# get all availabe characters
+# randomly pick character up to the length
+# ensure we have at least one of each character type
+# ensure length is valid
+
+import random
+import string
+
+
+def generate_password():
+    length = int(input("Enter the desired password length: ").strip())
+    include_uppercase = input("Include uppercase letters?(yes/no) ").strip().lower()
+    include_digit = input("Include digits?(yes/no) ").strip().lower()
+    include_special = input("Include special characters?(yes/no) ").strip().lower()
+
+    if length < 4:
+        print("Password length must be atleast 4 characters.")
+        return
+    
+    lower = string.ascii_lowercase
+    uppercase = string.ascii_uppercase if include_uppercase == "yes" else ""
+    special = string.punctuation if include_special == "yes" else ""
+    digit = string.digits if include_digit == "yes" else ""
+    all_characters = lower + uppercase + special + digit
+
+    print(all_characters)
+
+
+generate_password()
