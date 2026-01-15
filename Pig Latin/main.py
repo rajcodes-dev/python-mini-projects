b94d3c06d8d5d1addbf67523b766b34c36e1fdaa
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-15 09:30:27 +0530

    list to string

--- Pig Latin/main.py
+++ Pig Latin/main.py
@@ -8,13 +8,13 @@ pigLatin = []
 for word in message.split():
     # Separate the non-letters at the start of this word:
     prefixNonLetters = ''
-    while len(message) > 0 and not word[0].isalpha():
+    while len(word) > 0 and not word[0].isalpha():
         prefixNonLetters += word[0]
         word = word[1:]
     if len(word) == 0:
         pigLatin.append(prefixNonLetters)
         continue
-    
+
     # Separate the non-letters at the end of this word:
     suffixNonLetters = ''
     while not word[-1].isalpha():
@@ -22,8 +22,8 @@ for word in message.split():
         word = word[:-1]
 
     # Remember that the word are in title or upper case
+    wasTitle = word.istitle()
     wasUpper = word.isupper()
-    wasTitle = word.title()
 
     word = word.lower() # lower the word for easier translation
 
@@ -32,7 +32,7 @@ for word in message.split():
     while len(word) > 0 and not word[0] in VOWELS:
         prefixConsonants += word[0]
         word = word[1:]
-    
+
     # Add the pig latin at the ending to the word:
     if prefixConsonants != '':
         word += prefixConsonants + 'ay'
@@ -45,4 +45,8 @@ for word in message.split():
     if wasUpper:
         word = word.upper()
 
-    
+    # Add non-letter word at the start and the end of the word
+    pigLatin.append(prefixNonLetters + word + suffixNonLetters)
+
+# Join all the word back together into a single string:
+print(' '.join(pigLatin))
